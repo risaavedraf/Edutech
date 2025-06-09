7438cc6d598f66a278a45150e98ea6c1d7f3c0cf
r1cky-duoc <ri.saavedraf@duocuc.cl>
2025-06-09 11:02:23 -0400

    a

---
+++ msvc-cursos/src/main/java/com/edutech/msvc/cursos/init/LoadDatabase.java
@@ -0,0 +1,45 @@
+package com.edutech.msvc.cursos.init;
+
+import com.edutech.msvc.cursos.models.entities.Curso;
+import com.edutech.msvc.cursos.repositories.CursoRepository;
+import net.datafaker.Faker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.Locale;
+import java.util.Random;
+
+@Component
+public class LoadDatabase implements CommandLineRunner {
+
+    private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
+
+    @Autowired
+    CursoRepository cursoRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        Faker faker = new Faker(Locale.of("es","CL"));
+        Random rand = new Random();
+        int uno = 1;
+        int dos = 2;
+
+        if(cursoRepository.count() == 0){
+            for(int i=0;i<100;i++){
+                Curso curso = new Curso();
+                curso.setNombre(faker.educator().course());
+                curso.setDuracion(rand.nextLong());
+                curso.setFechaCreacion(faker.timeAndDate().birthday(uno, dos));
+                curso.setPrecio(rand.nextLong());
+                curso.setEstado(Boolean.TRUE);
+
+                log.info("El curso creado es {}", curso);
+            }
+        }
+
+    }
+}
--- msvc-cursos/src/main/java/com/edutech/msvc/cursos/repositories/CursosRepository.java
+++
@@ -1,9 +0,0 @@
-package com.edutech.msvc.cursos.repositories;
-
-import com.edutech.msvc.cursos.models.entities.Cursos;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CursosRepository extends JpaRepository<Cursos,Long> {
-}
